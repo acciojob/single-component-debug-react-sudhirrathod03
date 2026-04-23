aa245c1edbf97a7e2588d5d914114ae91f4d053d
Sudhir Rathod <125038605+sudhirrathod03@users.noreply.github.com>
2026-04-23 19:08:52 +0530

    Add id attribute to main div in App component

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import React from "react";
 
 function App() {
   return (
-    <div>
+    <div id="main">
      <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
     </div>
   );
